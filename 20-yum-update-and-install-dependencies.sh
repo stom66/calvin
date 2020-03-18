ed81f4bdd631407203c064d060f01a30befd3151
Tom Steventon <tomsteventon@gmail.com>
2020-03-18 03:06:40 +0000

    Fixed var names

--- 20-yum-update-and-install-dependencies.sh
+++ 20-yum-update-and-install-dependencies.sh
@@ -3,10 +3,10 @@
 # Runs YUM update and installs various dependencies
 
 PACKAGES=""
-PACKAGES="{$PACKAGES} screen wget nano epel-release gcc gcc-c++ gem git"
-PACKAGES="{$PACKAGES} lm_sensors make ncdu perl perl-Authen-PAM"
-PACKAGES="{$PACKAGES} perl-CPAN ruby-devel rubygems scl-utils util-linux"
-PACKAGES="{$PACKAGES} yum-utils zip"
+PACKAGES="${PACKAGES} screen wget nano epel-release gcc gcc-c++ gem git"
+PACKAGES="${PACKAGES} lm_sensors make ncdu perl perl-Authen-PAM"
+PACKAGES="${PACKAGES} perl-CPAN ruby-devel rubygems scl-utils util-linux"
+PACKAGES="${PACKAGES} yum-utils zip"
 
 yum update -y
 yum install -y $PACKAGES
