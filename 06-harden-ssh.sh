947fb002192d09e89d15c9b2b73dceb4c19ac951
Tom Steventon <tomsteventon@gmail.com>
2020-04-06 03:30:30 +0100

    SSH port and Postfix relay settings

--- 06-harden-ssh.sh
+++ 06-harden-ssh.sh
@@ -3,14 +3,23 @@
 # Configures SSHD to force keypair logins, disables root and password logins
 declare PREFIX="Calvin | harden-ssh |"
 
+# Change the port used for SSH
+if [ ! -z "$1" ]; then
+	echo "${PREFIX} SSH port changed to $1" >> ./calvin.log
+	sed -i 's/#\?\(Port\s*\).*$/\1 ${1}/' /etc/ssh/sshd_config
+fi
 
-#sed -i 's/#\?\(Port\s*\).*$/\1 2231/' /etc/ssh/sshd_config
+# Disable weak authentication
 sed -i 's/#\?\(ChallengeResponseAuthentication\s*\).*$/\1 no/' /etc/ssh/sshd_config
 sed -i 's/#\?\(PasswordAuthentication\s*\).*$/\1 no/' /etc/ssh/sshd_config
+
+# Disable root logins
 sed -i 's/#\?\(PermitRootLogin\s*\).*$/\1 no/' /etc/ssh/sshd_config
+
+# Enable PAM
 sed -i 's/#\?\(UsePAM\s*\).*$/\1 yes/' /etc/ssh/sshd_config
 
-echo "${PREFIX} Disabled ssh password auth and root logins" >> ./calvin.log
+echo "${PREFIX} SSH Hardened" >> ./calvin.log
 
 # Restart SSH Daemon
 systemctl reload sshd
--- 100-harden-postfix.sh
+++ 100-harden-postfix.sh
@@ -9,3 +9,10 @@ postconf -e 'disable_vrfy_command=yes'
 
 # Force HELO required
 postconf -e 'smtpd_helo_required = yes'
+
+# Set recipient, sender, security and relay restrictions
+postconf -e 'smtpd_recipient_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination, reject_rbl_client zen.spamhaus.org, reject_rhsbl_helo dbl.spamhaus.org, reject_rhsbl_sender dbl.spamhaus.org'
+postconf -e 'smtpd_relay_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination'
+
+postconf -e 'smtpd_sasl_security_options = noanonymous'
+postconf -e 'smtpd_sender_restrictions = reject_unknown_sender_domain'
