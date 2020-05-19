7e7efad27c4fa174d16541aee623b1ae86ac0202
Tom Steventon <tomsteventon@gmail.com>
2020-05-19 18:56:01 +0100

    Moved autoconfig and autodiscover to resources/

--- 75-virtualmin-features.sh
+++ 75-virtualmin-features.sh
@@ -19,6 +19,7 @@ virtualmin set-global-feature --default-off virtualmin-dav
 echo "${PREFIX} DAV disabled" >> ./calvin.log
 
 # Change autoconfig script to have hard-coded STARTTLS
-virtualmin modify-template --id 0 --setting autoconfig --value '<?xml version="1.0" encoding="UTF-8"?><clientConfig version="1.1"><emailProvider id="$SMTP_DOMAIN"> <domain>$SMTP_DOMAIN</domain><displayName>$OWNER Email</displayName> <displayShortName>$OWNER</displayShortName> <incomingServer type="imap"> <hostname>$IMAP_HOST</hostname> <port>$IMAP_PORT</port> <socketType>$IMAP_TYPE</socketType> <authentication>$IMAP_ENC</authentication> <username>$SMTP_LOGIN</username></incomingServer> <incomingServer type="pop3"> <hostname>$IMAP_HOST</hostname> <port>$POP3_PORT</port> <socketType>$IMAP_TYPE</socketType> <authentication>$POP3_ENC</authentication> <username>$SMTP_LOGIN</username></incomingServer> <outgoingServer type="smtp"><hostname>$SMTP_HOST</hostname><port>$SMTP_PORT</port> <socketType>STARTTLS</socketType> <authentication>$SMTP_ENC</authentication> <username>$SMTP_LOGIN</username> </outgoingServer></emailProvider></clientConfig>'
+virtualmin modify-template --id 0 --setting autoconfig --value "$(cat ./resources/autoconfig.xml | tr '\n' '')"
+virtualmin modify-template --id 0 --setting autodiscover --value "$(cat ./resources/autodiscover.xml | tr '\n' '')"
 virtualmin modify-mail --all-domains --autoconfig
 echo "${PREFIX} autoconfig enabled and STARTTLS hard-coded" >> ./calvin.log
