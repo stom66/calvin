464bdaf3c24ea6aa16709a8a10081e4fe180d104
Cloud User <centos@ls2.tom2.co.uk>
2020-10-01 00:27:45 +0000

    Autoconfig/autodiscover tweaks

--- 75-virtualmin-features.sh
+++ 75-virtualmin-features.sh
@@ -19,7 +19,7 @@ virtualmin set-global-feature --default-off virtualmin-dav
 echo "${PREFIX} DAV disabled" >> ./calvin.log
 
 # Change autoconfig script to have hard-coded STARTTLS
-virtualmin modify-template --id 0 --setting autoconfig --value "$(cat ./resources/autoconfig.xml | tr '\n' '')"
-virtualmin modify-template --id 0 --setting autodiscover --value "$(cat ./resources/autodiscover.xml | tr '\n' '')"
 virtualmin modify-mail --all-domains --autoconfig
+sudo virtualmin modify-template --id 0 --setting autoconfig --value "$(cat ./resources/autoconfig.xml | tr '\n' ' ')"
+sudo virtualmin modify-template --id 0 --setting autodiscover --value "$(cat ./resources/autodiscover.xml | tr '\n' ' ')"
 echo "${PREFIX} autoconfig enabled and STARTTLS hard-coded" >> ./calvin.log
